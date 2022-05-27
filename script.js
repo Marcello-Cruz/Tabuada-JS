34264fe77e8c60a9d7517ec7583e579f5e5b1eed
MarcelloB2 <marcellinhopastelzinho@gmail.com>
2022-05-26 21:41:35 -0300

    add maxima da programaçao: siga a mare

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const numero = Number(prompt('Digite um número para a sua tabuada', '0'));
+alert ('Digite um número para a sua tabuada')
 if (Object.is(numero, NaN)){
-    alert("Digite somente números");
+    alert ('Digite somente números');
     }else{
         let resultado = `Tabuada do ${numero}: \n`;
         for (let i = 0; i <= 10; i++){
