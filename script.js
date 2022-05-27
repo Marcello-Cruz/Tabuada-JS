bad903c9824bc62703214e7185f6c9d1947576a1
MarcelloB2 <marcellinhopastelzinho@gmail.com>
2022-05-27 19:50:29 -0300

    conserta o link pro script do java no html]

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const numero = Number(prompt('Digite um número para a sua tabuada', '0'));
-alert ('Digite um número para a sua tabuada')
 if (Object.is(numero, NaN)){
-    alert ('Digite somente números');
     }else{
         let resultado = `Tabuada do ${numero}: \n`;
         for (let i = 0; i <= 10; i++){
             resultado = `${resultado}\n${numero} x ${i} = ${numero * i}`;
         }
-        alert(resultado);
+alert(resultado);
     }
