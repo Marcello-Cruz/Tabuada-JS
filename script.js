ce46fdfb506bcd4ed8c438c34ec5a7db4770dbcf
MarcelloB2 <marcellinhopastelzinho@gmail.com>
2022-05-26 21:32:49 -0300

    add exerc finalizado

---
+++ script.js
@@ -0,0 +1,10 @@
+const numero = Number(prompt('Digite um número para a sua tabuada', '0'));
+if (Object.is(numero, NaN)){
+    alert("Digite somente números");
+    }else{
+        let resultado = `Tabuada do ${numero}: \n`;
+        for (let i = 0; i <= 10; i++){
+            resultado = `${resultado}\n${numero} x ${i} = ${numero * i}`;
+        }
+        alert(resultado);
+    }
